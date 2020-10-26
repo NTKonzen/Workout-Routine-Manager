28257b33b01262160b350b49330a1796a3206c2e
Nicholas Konzen <nicholaskonzen@gmail.com>
2020-10-26 12:05:15 -0700

    Built Workout model

---
+++ models/Workout.js
@@ -0,0 +1,44 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const exerciseSchema = new Schema({
+    "type": {
+        type: String,
+        trim: true,
+        required: 'String is required'
+    },
+    name: {
+        type: String,
+        trim: true,
+        required: 'String is required'
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    weight: {
+        type: Number,
+        required: true
+    },
+    reps: {
+        type: Number,
+        required: true
+    },
+    sets: {
+        type: Number,
+        required: true
+    }
+})
+
+const workoutSchema = new Schema({
+    date: {
+        type: Date,
+        default: Date.now()
+    },
+    exercises: [exerciseSchema]
+});
+
+const Workout = mongoose.model('Workout', workoutSchema);
+
+module.exports = Workout;
