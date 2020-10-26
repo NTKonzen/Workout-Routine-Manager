735b5db2e75cb36f53ede19bb1bd7d425736f30f
Nicholas Konzen <nicholaskonzen@gmail.com>
2020-10-26 09:20:10 -0700

    Built basic server.js minus routes

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose')
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true });
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => {
+    console.log(`App is listening on PORT ${PORT}`);
+});
